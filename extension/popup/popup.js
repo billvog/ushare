95f49bab9c254471fc11b2d8a01ad12579563788
billvog <billvog04@gmail.com>
2021-01-30 11:52:22 +0200

    Organized hierarchy

---
+++ extension/popup/popup.js
@@ -0,0 +1,16 @@
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+    // Get current tab's url
+    const url = tabs[0].url;
+
+    // Validate url length
+    if (url.length > 900) {
+        document.querySelector('#qr-code-image').setAttribute('title', 'Cannot generate QR Code');
+        document.querySelector('.status-message').textContent = 'This url is too big and a QR Code cannot be generated';
+        return;
+    }
+
+    // Show QR Code
+    document.querySelector('#qr-code-image').src = `http://api.qrserver.com/v1/create-qr-code/?data=${url}&size=200x200`
+    document.querySelector('#qr-code-image').setAttribute('title', url);
+    document.querySelector('.status-message').textContent = 'Your QR Code is ready!';
+});
